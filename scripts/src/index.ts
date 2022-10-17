e54fbd63d5a688f2c65f84a9362931fac32a9275
Charles Liu <charles.liu0309@gmail.com>
2022-10-16 23:58:14 -0400

    added neo4j connection

--- scripts/src/index.ts
+++ scripts/src/index.ts
@@ -2,6 +2,7 @@ import fs from 'fs';
 
 import csv from 'csv-parser';
 import * as dotenv from 'dotenv';
+import neo4j, {Driver} from 'neo4j-driver';
 import {TwitterApi} from 'twitter-api-v2';
 
 import type {User} from './types';
@@ -24,11 +25,53 @@ function readCSV() {
     });
 }
 
+async function initDb() {
+  return neo4j.driver(
+    'bolt://localhost:7687',
+    neo4j.auth.basic('neo4j', 'development')
+  );
+}
+
+async function populateDb(driver: Driver) {
+  const session = driver.session({
+    database: 'neo4j',
+    defaultAccessMode: neo4j.session.WRITE,
+  });
+  console.log(session);
+
+  // Run a Cypher statement, reading the result in a streaming manner as records arrive:
+  session
+    .run('MERGE (alice:Person {name : $nameParam}) RETURN alice.name AS name', {
+      nameParam: 'Alice',
+    })
+    .subscribe({
+      onKeys: keys => {
+        console.log(keys);
+      },
+      onNext: record => {
+        console.log(record.get('name'));
+      },
+      onCompleted: () => {
+        session.close(); // returns a Promise
+      },
+      onError: error => {
+        console.log(error);
+      },
+    });
+}
+
+async function closeDb(driver: Driver) {
+  await driver.close();
+}
+
 async function main() {
   // Play with the built in methods
   const user = await readOnlyClient.v2.userByUsername('CharlesLiu9');
-  console.log(user);
-  readCSV();
+  // readCSV();
+  const driver = await initDb();
+  console.log(driver);
+  await populateDb(driver);
+  await closeDb(driver);
 }
 
 main();
