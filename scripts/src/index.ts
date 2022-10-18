707908ab2922e278b6468870e01259b196d8eb6d
Charles Liu <charles.liu0309@gmail.com>
2022-10-18 01:48:07 -0400

    updates neo4j users with semi populated data from twitter api in batches

--- scripts/src/index.ts
+++ scripts/src/index.ts
@@ -1,11 +1,8 @@
-import fs from 'fs';
-
-import csv from 'csv-parser';
 import * as dotenv from 'dotenv';
-import neo4j, {Driver} from 'neo4j-driver';
 import {TwitterApi} from 'twitter-api-v2';
 
-import type {User} from './types';
+import {initDb, populateDb, closeDb} from './db';
+import {getTwitterDataAndUpdateDb} from './twitter';
 
 dotenv.config();
 
@@ -15,49 +12,10 @@ const twitterClient = new TwitterApi(process.env.TWITTER_API_BEARER as string);
 // Tell typescript it's a readonly app
 const readOnlyClient = twitterClient.readOnly;
 
-function readCSV() {
-  const results: User[] = [];
-  fs.createReadStream('users.csv')
-    .pipe(csv())
-    .on('data', data => results.push(data))
-    .on('end', () => {
-      console.log(results);
-    });
-}
-
-async function initDb() {
-  return neo4j.driver(
-    'bolt://localhost:7687',
-    neo4j.auth.basic('neo4j', 'development')
-  );
-}
-
-async function populateDb(driver: Driver) {
-  const session = driver.session({
-    database: 'neo4j',
-    defaultAccessMode: neo4j.session.WRITE,
-  });
-
-  // Run a Cypher statement, reading the result in a streaming manner as records arrive:
-  const res = await session.run(
-    'MERGE (alice:Person {name : $nameParam}) RETURN alice.name AS name',
-    {
-      nameParam: 'Alice',
-    }
-  );
-
-  console.log(res);
-}
-
-async function closeDb(driver: Driver) {
-  await driver.close();
-}
-
 async function main() {
-  const user = await readOnlyClient.v2.userByUsername('CharlesLiu9');
-  // readCSV();
   const driver = await initDb();
   await populateDb(driver);
+  await getTwitterDataAndUpdateDb(driver);
   await closeDb(driver);
 }
 
