f48c0d866711d96500a6990c1c093d9fee962ae0
Charles Liu <charles.liu0309@gmail.com>
2022-11-17 05:27:38 -0500

    successfully fetched tweets

--- scripts/src/db.ts
+++ scripts/src/db.ts
@@ -1,27 +1,28 @@
 import * as dotenv from 'dotenv';
-import neo4j, {Driver} from 'neo4j-driver';
+import neo4j, {Driver, Session} from 'neo4j-driver';
 
 dotenv.config();
 
 export async function initDb() {
-  return neo4j.driver(
+  const driver = neo4j.driver(
     process.env.NEO4J_URI as string,
     neo4j.auth.basic(
       process.env.NEO4J_USERNAME as string,
       process.env.NEO4J_PASSWORD as string
     )
   );
-}
-
-export async function populateDb(driver: Driver) {
   const session = driver.session({
     database: process.env.NEO4J_DB as string,
     defaultAccessMode: neo4j.session.WRITE,
   });
 
+  return {driver, session};
+}
+
+export async function populateDb(session: Session) {
   try {
     const res = await session.run(
-      "LOAD CSV WITH HEADERS FROM 'file:///production-users.csv' AS line \
+      "LOAD CSV WITH HEADERS FROM 'file:///production-users-small.csv' AS line \
       CREATE (:User {username: line.twitter, name: line.name, domain: line.domain});"
     );
   } catch (err) {
--- scripts/src/twitter.ts
+++ scripts/src/twitter.ts
@@ -1,26 +1,22 @@
 import * as dotenv from 'dotenv';
-import neo4j, {Driver} from 'neo4j-driver';
+import {Session} from 'neo4j-driver';
 import {chunk} from 'lodash';
 import {TwitterApi} from 'twitter-api-v2';
 
-import {sleep} from './utils';
+import {outputProgress, sleep} from './utils';
 
 dotenv.config();
 
-export async function getTwitterDataAndUpdateDb(driver: Driver) {
-  const session = driver.session({
-    database: process.env.NEO4J_DB as string,
-    defaultAccessMode: neo4j.session.WRITE,
-  });
-
+export async function getTwitterDataAndUpdateDb(session: Session) {
   // Instantiate with desired auth type (here's Bearer v2 auth)
   const twitterClient = new TwitterApi(
     process.env.TWITTER_API_BEARER as string
   );
 
   // Tell typescript it's a readonly app
-  const readOnlyClient = twitterClient.readOnly;
+  const client = twitterClient.readOnly;
 
+  // fetching users
   try {
     const res = await session.run('MATCH (u:User) RETURN u;');
 
@@ -31,8 +27,8 @@ export async function getTwitterDataAndUpdateDb(driver: Driver) {
     // Getting additional data for each user
     // Twitter rate limits - load slowly & batch
     const batchUsernames = chunk(usernames, 100);
-    for (const batchUsername of batchUsernames) {
-      const res = await readOnlyClient.v2.usersByUsernames(batchUsername, {
+    for (const [i, usernames] of batchUsernames.entries()) {
+      const res = await client.v2.usersByUsernames(usernames, {
         'user.fields': 'public_metrics,created_at',
         expansions: 'pinned_tweet_id',
       });
@@ -56,21 +52,79 @@ export async function getTwitterDataAndUpdateDb(driver: Driver) {
       for (const user of users) {
         const res = await session.run(
           'MATCH (u:User {username: $usernameParam}) \
-          SET u += { followers_count: $followersCountParam, following_count: $followingCountParam, tweet_count: $tweetCountParam, created_at: $createdAtParam} \
+          SET u += { twitter_id: $twitterIdParam, name: $name, followers_count: $followersCountParam, following_count: $followingCountParam, tweet_count: $tweetCountParam, created_at: $createdAtParam} \
           RETURN u',
           {
             usernameParam: user.username,
+            twitterIdParam: +user.id,
+            name: user.name,
             followersCountParam: +(user.public_metrics?.followers_count ?? 0),
             followingCountParam: +(user.public_metrics?.following_count ?? 0),
             tweetCountParam: +(user.public_metrics?.tweet_count ?? 0),
-            createdAtParam: +new Date(user.created_at ?? '0001').getFullYear(),
+            createdAtParam: new Date(user.created_at ?? '0001').getTime(), // stores as ms, RETURN date({ epochMillis: $startsAt }) to convert to Date in cypher
           }
         );
         // console.log(res.records.map(record => record.get('u').properties));
       }
-      await sleep(300000); // 5 minutes
+      outputProgress(
+        i * 100 + usernames.length,
+        i === batchUsernames.length - 1
+      );
+      if (i !== batchUsernames.length - 1) await sleep(300000); // 5 minutes
     }
   } catch (err) {
-    console.log(err);
+    console.log('Unabled to get all users:', err);
+  }
+
+  // fetching tweets
+  let curName = '';
+  try {
+    const res = await session.run('MATCH (u:User) RETURN u;');
+
+    const accounts: {name: string; twitter_id: string | undefined}[] =
+      res.records.map(record => ({
+        name: record.get('u').properties.name,
+        twitter_id: record.get('u').properties.twitter_id,
+      }));
+
+    // calling api
+    for (const [i, account] of accounts.entries()) {
+      if (!account.twitter_id) break;
+      curName = account.name;
+
+      const timeline = await client.v2.userTimeline(
+        account.twitter_id.toString(),
+        {
+          'tweet.fields': ['author_id', 'created_at', 'id', 'text'],
+          'media.fields': ['alt_text'],
+        }
+      );
+
+      // adding most recent 3 tweets to db
+      let count = 0;
+      for await (const tweet of timeline) {
+        if (tweet.author_id === account.twitter_id.toString()) {
+          const res = await session.run(
+            'CREATE (t:Tweet {tweet_id: $tweetIdParam, text: $textParam, createdAt: $createdAtParam}) \
+            WITH t \
+            MATCH (u:User {twitter_id: $twitterIdParam}) \
+            CREATE (u)-[r:Author]->(t);',
+            {
+              tweetIdParam: +tweet.id,
+              textParam: tweet.text,
+              twitterIdParam: +tweet.author_id,
+              createdAtParam: new Date(tweet.created_at ?? '0001').getTime(),
+            }
+          );
+        }
+        count++;
+        if (count === 2) break;
+      }
+
+      outputProgress(i + 1, i === accounts.length - 1, curName);
+      if (i !== accounts.length - 1) await sleep(3100); // 3.1 seconds
+    }
+  } catch (err) {
+    console.log(`Failed to get tweets for ${curName}:`, err);
   }
 }
